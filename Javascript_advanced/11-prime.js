9d131503b61b92e7f1c9c82f257b0643e759ab1f
root <i.kewa@alustudent.com>
2024-11-07 10:14:10 +0000

    task 11

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,28 @@
+function countPrimeNumbers() {
+    let count = 0;
+
+    function isPrime(num) {
+        if (num < 2) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+
+    for (let i = 2; i <= 100; i++) {
+        if (isPrime(i)) count++;
+    }
+
+    return count;
+}
+
+
+setTimeout(() => {
+    const startTime = performance.now();
+    for (let i = 0; i < 100; i++) {
+        countPrimeNumbers();
+    }
+    const endTime = performance.now();
+    console.log(`Execution time of calculating prime numbers 100 times was ${endTime - startTime} milliseconds.`);
+}, 0);
+
